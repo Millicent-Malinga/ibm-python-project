3b118ce4481631086202434f79746d12f549befb
Millicent-Malinga <132472427+Millicent-Malinga@users.noreply.github.com>
2023-05-03 19:02:36 +0200

    Add files via upload

---
+++ xzceb-flask_eng_fr/machinetranslation/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+import translator
+class TestTranslator(unittest.TestCase):
+    def test_nullinputs(self):
+        self.assertEqual(translator.english_to_french(''), '')
+        self.assertEqual(translator.french_to_english(''), '')
+
+    def test_greetings(self):
+        self.assertEqual(translator.french_to_english('Bonjour'), 'Hello')
+        self.assertEqual(translator.english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(translator.english_to_french('Hello'), 'Hello')
+        self.assertNotEqual(translator.french_to_english('Bonjour'), 'Bonjour')
+
+    
+if __name__ == "__main__":
+     unittest.main()
